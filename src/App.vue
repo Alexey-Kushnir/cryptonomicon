a574e2b79b7b78f8a3a78b75352fb9185ee46b18
Alexey Kushnir <al7787@gmail.com>
2024-07-15 21:58:53 +0300

    change fetch to ws

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script>
+import { subscribeToTicker, unsubscribeFromTicker } from './api';
+
 export default {
   name: 'App',
   data() {
@@ -16,6 +18,14 @@ export default {
     const windowData = Object.fromEntries(
       new URL(window.location).searchParams.entries()
     );
+    // const VALID_KEYS = ["filter", "page"];
+
+    // VALID_KEYS.forEach(key => {
+    //   if (windowData[key]) {
+    //     this[key] = windowData[key];
+    //   }
+    // });
+
     if (windowData.filter) {
       this.filter = windowData.filter;
     }
@@ -26,8 +36,14 @@ export default {
     const tickersData = localStorage.getItem('cryptonomicon-list');
     if (tickersData) {
       this.tickers = JSON.parse(tickersData);
-      this.tickers.forEach(({ name }) => this.subscribeToUpdates(name));
+      this.tickers.forEach((ticker) => {
+        subscribeToTicker(ticker.name, (newPrice) =>
+          this.updateTicker(ticker.name, newPrice)
+        );
+      });
     }
+
+    // setInterval(this.updateTickers, 5000);
   },
   computed: {
     startIndex() {
@@ -65,35 +81,33 @@ export default {
     },
   },
   methods: {
-    subscribeToUpdates(tickerName) {
-      setInterval(async () => {
-        try {
-          const res = await fetch(
-            `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=37939f7099f6e733c6cf9f7664a23d49d8cb5e592ad10bafa19eac633a9c2629`
-          );
-          const data = await res.json();
-          if (data.USD !== undefined) {
-            this.tickers.find((t) => t.name === tickerName).price =
-              data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+    async updateTicker(tickerName, price) {
+      this.tickers
+        .filter((t) => t.name === tickerName)
+        .forEach((t) => {
+          if (t === this.selectedTicker) {
+            this.graph.push(price);
           }
-          if (this?.selectedTicker?.name === tickerName) {
-            this.graph.push(data.USD);
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      }, 5000);
-      this.ticker = '';
+          t.price = price;
+        });
+    },
+    formatPrice(price) {
+      if (price === '-') {
+        return price;
+      }
+      return price > 1 ? price.toFixed(2) : price.toPrecision(2);
     },
     add() {
       const currentTicker = {
         name: this.ticker.toUpperCase(),
-        price: '_',
+        price: '-',
       };
       this.tickers = [...this.tickers, currentTicker];
       this.filter = '';
-      // localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers));
-      this.subscribeToUpdates(currentTicker.name);
+      this.ticker = '';
+      subscribeToTicker(currentTicker.name, (newPrice) =>
+        this.updateTicker(currentTicker.name, newPrice)
+      );
     },
     select(ticker) {
       this.selectedTicker = ticker;
@@ -101,14 +115,15 @@ export default {
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter((t) => t.name !== tickerToRemove.name);
       if (this.selectedTicker === tickerToRemove) this.selectedTicker = null;
+      unsubscribeFromTicker(tickerToRemove.name);
     },
   },
   watch: {
-    tickers() {
-      localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers));
-    },
     selectedTicker() {
       this.graph = [];
+    },
+    tickers() {
+      localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers));
     },
     paginatedTickers() {
       if (this.paginatedTickers.length === 0 && this.page > 1) this.page -= 1;
@@ -225,7 +240,7 @@ export default {
                 {{ t.name }} - USD
               </dt>
               <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                {{ t.price }}
+                {{ formatPrice(t.price) }}
               </dd>
             </div>
             <div class="w-full border-t border-gray-200"></div>
